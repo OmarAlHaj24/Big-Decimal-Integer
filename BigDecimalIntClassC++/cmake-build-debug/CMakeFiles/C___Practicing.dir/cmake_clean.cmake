b4fcbd144b68a46ebd58139f70d1251ba6a58cab
OmarAlHaj24 <52503015+OmarAlHaj24@users.noreply.github.com>
2021-06-13 09:42:36 +0200

    Add files via upload

---
+++ BigDecimalIntClassC++/cmake-build-debug/CMakeFiles/C___Practicing.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C___Practicing.dir/main.cpp.obj"
+  "C___Practicing.exe"
+  "C___Practicing.exe.manifest"
+  "C___Practicing.pdb"
+  "libC___Practicing.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C___Practicing.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
